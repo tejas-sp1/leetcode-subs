9837fcff09b5696e5a9d37190e49ea6149395db2
Tejas S P <tejas516sp@gmail.com>
2026-07-17 22:02:35 +0530

    Merge remote changes

--- water container.py
+++ water container.py
@@ -16,3 +16,27 @@ class Solution:
                 rightMax = max(rightMax, height[r])
                 res += rightMax - height[r]
         return res
+
+
+
+
+solution 2
+
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        if not height:
+            return 0
+        n = len(height)
+        res = 0
+
+        for i in range(n):
+            leftMax = rightMax = height[i]
+
+            for j in range(i):
+                leftMax = max(leftMax, height[j])
+            for j in range(i + 1, n):
+                rightMax = max(rightMax, height[j])
+
+            res += min(leftMax, rightMax) - height[i]
+        return res
