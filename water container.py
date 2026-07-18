667bda73bf57df861e271b80937eae7c459c9ee5
Tejas S P <tejas516sp@gmail.com>
2026-07-18 19:23:18 +0530

    Describe your changes

--- water container.py
+++ water container.py
@@ -1,42 +1,21 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        if not height:
-            return 0
-
-        l, r = 0, len(height) - 1
-        leftMax, rightMax = height[l], height[r]
-        res = 0
-        while l < r:
-            if leftMax < rightMax:
-                l += 1
-                leftMax = max(leftMax, height[l])
-                res += leftMax - height[l]
-            else:
-                r -= 1
-                rightMax = max(rightMax, height[r])
-                res += rightMax - height[r]
-        return res
-
-
-
-
-solution 2
-
-
-class Solution:
-    def trap(self, height: List[int]) -> int:
-        if not height:
-            return 0
         n = len(height)
+        if n == 0:
+            return 0
+
+        leftMax = [0] * n
+        rightMax = [0] * n
+
+        leftMax[0] = height[0]
+        for i in range(1, n):
+            leftMax[i] = max(leftMax[i - 1], height[i])
+
+        rightMax[n - 1] = height[n - 1]
+        for i in range(n - 2, -1, -1):
+            rightMax[i] = max(rightMax[i + 1], height[i])
+
         res = 0
-
         for i in range(n):
-            leftMax = rightMax = height[i]
-
-            for j in range(i):
-                leftMax = max(leftMax, height[j])
-            for j in range(i + 1, n):
-                rightMax = max(rightMax, height[j])
-
-            res += min(leftMax, rightMax) - height[i]
+            res += min(leftMax[i], rightMax[i]) - height[i]
         return res
